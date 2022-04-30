00ea5748eea46f2a23750fb8bfa8dec92b1c0719
suvobank <secretsubha09@gmail.com>
2022-04-30 11:26:10 +0530

    Newsdog first structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        This is my first class. with Github!!
+        <Navbar/>
+        <News/>
+        <News/>
+        <News/>
+        <News/>
+        <News/>
+        
       </div>
     )
   }
