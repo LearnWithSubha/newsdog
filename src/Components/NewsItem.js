a928c00c8c0530fa7de0c4771698d09302748ff9
suvobank <secretsubha09@gmail.com>
2022-05-03 12:59:43 +0530

    API data fetched

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
-      <div>
-        this is a news item!!
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} rel="noreferrer noopener" target="_blank" className="btn btn-sm btn-primary">
+              Read me
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
